d0f702d2f61564e37a1cf004bb4a81db9ea34d89
cryocaustik <cryocaustik@gmail.com>
2018-03-09 22:04:59 -0800

    dark theme :green_heart:

---
+++ README.md
@@ -0,0 +1,18 @@
+# Slack Windows Client Themes
+
+Simple method to apply custom themes (e.g. dark theme 💜) to the Windows Slack client.
+
+## Method
+
+1. navigate to `%username%\AppData\Local\slack\app-3.1.0\resources\app.asar.unpacked\src\static`
+2. find and open file `ssb-interop.js` with a text editor
+3. paste the contents of `slack_theme.js` to the end of the file
+4. customize the `tt__customCss` section as you see fit (font color, background color, etc.)
+5. while in Slack client, press `cntrl+r` to reload the client
+6. enjoy your retinas!
+
+
+## Credit
+
+Core themes obtained from: [laCour/slack-night-mode](https://github.com/laCour/slack-night-mode)
+Method for attaching to client from: [foxus97 gist comment](https://gist.github.com/DrewML/0acd2e389492e7d9d6be63386d75dd99#gistcomment-2358430)
---
+++ slack_theme.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  let tt__customCss = `
+    // you can customize this section with your own color prefs. 
+    body,
+    .channel_header,
+    #footer,
+    .channel_title_info,
+    #channel_topic_text {
+          background: rgb(29, 32, 33);
+    }
+    
+    .c-message__body {
+          color: rgb(153, 174, 177);
+    }
+    
+    #team_menu,
+    .p-channel_sidebar {
+          background: #17191a !important;
+    }
+    
+    .c-presence--active {
+          color: rgb(177, 202, 17) !important;
+    }
+    
+    nav.p-channel_sidebar .p-channel_sidebar__channel--selected,
+    .p-channel_sidebar__link--selected,
+    .c-message_list__day_divider__label__pill,
+    .p-message_pane .c-message_list.c-virtual_list--scrollbar>.c-scrollbar__hider:before {
+          color: #eee !important;
+          background: rgb(27, 139, 210) !important;
+    }
+    
+    .c-message_list__day_divider__line {
+          border-top-color: rgb(27, 139, 210) !important
+    }
+    
+    #msg_input,
+    #primary_file_button {
+          background: #17191a !important;
+    }
+    
+    #msg_form #msg_input {
+          border-color: transparent;
+    }
+  `;
+  $.ajax({
+      url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+      success: function(css) {
+          $('<style></style>').appendTo('head').html(css + tt__customCss);
+      }
+  });
+});
