66871f1339e5d9e0327a509b0577d6b30f633138
Aleksandr Skobelev <Aleksandr.Skobelev@chpw.org>
2018-04-17 09:20:49 -0700

    temp fix for white msg bg

--- slack_theme.js
+++ slack_theme.js
@@ -43,6 +43,11 @@ document.addEventListener('DOMContentLoaded', function () {
         #msg_form #msg_input {
         border-color: transparent;
         }
+
+        // temp fix for white chat background from recent slack-app update
+        .p-message_pane .c-message_list.c-virtual_list--scrollbar > .c-scrollbar__hider {
+        background: #222; !important
+        }
     `;
     $.ajax({
         url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
